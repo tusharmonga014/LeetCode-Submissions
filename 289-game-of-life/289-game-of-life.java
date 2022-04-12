ab8a8581240b078308b279789de6d7a2f566c3ec
Tushar Monga <tusharmongatt@gmail.com>
2022-04-12 22:16:27 +0530

    Time: 1 ms (55.24%), Space: 42.1 MB (69.86%) - LeetHub

--- 289-game-of-life/289-game-of-life.java
+++ 289-game-of-life/289-game-of-life.java
@@ -1,40 +1,35 @@
 class Solution {
-    public void gameOfLife(int[][] board) {
-        int[][] newBoard = new int[board.length][board[0].length];
-        
-        int dir[][] = {{-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}};
-        
-        for(int i = 0; i < board.length; i++) {
-            for(int j = 0; j < board[0].length; j++) {
-                int countLiveNei = 0;
-                for(int d = 0; d < dir.length; d++) {
-                    int x = i + dir[d][0];
-                    int y = j + dir[d][1];
-                    
-                    if(x >= 0 && y >= 0 && x < board.length && y < board[0].length) {
-                        if(board[x][y] == 1)
-                            countLiveNei++;
-                    }
+    public void gameOfLife(int[][] A) {
+        int m= A.length, n=A[0].length;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int res= count(A,i,j,m,n); //count live neighbours
+                //original alive
+                if(A[i][j]==1){
+                    if(res<2 || res>3) A[i][j]=-A[i][j];   
                 }
-                
-                if(board[i][j] == 0) {
-                    if(countLiveNei == 3) {
-                        newBoard[i][j] = 1;
-                    }
-                } else {
-                    if(countLiveNei < 2 || countLiveNei > 3) {
-                        newBoard[i][j] = 0;
-                    } else {
-                        newBoard[i][j] = 1;
-                    }
+                else{
+                    A[i][j]=2;  //make it 2 for identifying it as a dead cell
+                    if(res!=3) A[i][j]=-A[i][j]; //make -2 if still dead
                 }
             }
         }
-        
-        for(int i = 0; i < board.length; i++) {
-            for(int j = 0; j < board[0].length; j++) {
-                board[i][j] = newBoard[i][j];
-            }
+
+        //convert all -ve to 0 and +ve to 1
+        for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++) A[i][j]= A[i][j]<0? 0 : 1;
+    }
+
+    //indices of neighbours in 8 direction
+    int dis[][]={{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
+
+    //function for counting the live neighbours of current cell
+    int count(int[][] A,int i,int j,int m,int n){
+        int cnt=0;
+        for(int k=0;k<8;k++){
+            int x= i+dis[k][0], y= j+dis[k][1];
+            if(x>=0 && y>=0 && x<m && y<n && Math.abs(A[x][y])==1) cnt++;
         }
+        return cnt;
     }
 }
