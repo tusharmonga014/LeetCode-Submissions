7498b466ba1f87062d6010429810a761295ea781
Tushar Monga <tusharmongatt@gmail.com>
2022-04-29 10:09:19 +0530

    Time: 1 ms (83.33%), Space: 54.2 MB (23.47%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,72 +1,16 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
         
-//         // two sets
-//         HashSet<Integer> s1 = new HashSet<>();
-//         HashSet<Integer> s2 = new HashSet<>();
+        // two sets
+        s1 = new boolean[graph.length];
+        s2 = new boolean[graph.length];
         
-//         // queue for bfs
-//         LinkedList<Integer> que = new LinkedList<>();
-        
-//         // for marking visited nodes
-//         boolean vis[] = new boolean[graph.length];
-        
-//         // total nodes visited
-//         int tnv = 0;
-        
-//         s1.add(0);
-//         que.addLast(0);
-        
-//         while(tnv <= graph.length) {
-            
-//             if(que.isEmpty()) {
-//                 for(int i = 0; i < vis.length; i++) {
-//                     if(!vis[i]) {
-//                         que.addLast(i);
-//                         break;
-//                     }
-//                 }
-//             }
-            
-//             int rem = que.removeFirst();
-            
-//             vis[rem] = true;
-//             tnv++;
-            
-//             if(s1.contains(rem)) {
-//                 for(int i = 0; i < graph[rem].length; i++) {
-//                     int v = graph[rem][i];
-//                     if(s1.contains(v)) {
-//                         return false;  
-//                     } else {
-//                         if(!vis[v])
-//                             que.addLast(v);
-//                         s2.add(v);
-//                     }
-//                 }
-//             } else {
-//                 for(int i = 0; i < graph[rem].length; i++) {
-//                     int v = graph[rem][i];
-//                     if(s2.contains(v)) {
-//                         return false;  
-//                     } else {
-//                         if(!vis[v])
-//                             que.addLast(v);
-//                         s1.add(v);
-//                     }
-//                 }
-//             }
-   
-//         }
-        
-//         return true;
-        
-        s1 = new HashSet<>();
-        s2 = new HashSet<>();
+        // for already visited
         boolean vis[] = new boolean[graph.length];
+        
         for(int i = 0; i < graph.length; i++) {
             if(!vis[i]) {
-                s1.add(i);
+                s1[i] = true;
                 boolean res = dfs(graph, i, vis);
                 if(!res) {
                     return false;
@@ -77,27 +21,27 @@ class Solution {
         return true;
     }
     
-    HashSet<Integer> s1 = new HashSet<>();
-    HashSet<Integer> s2 = new HashSet<>();
+    boolean s1[];
+    boolean s2[];
     
     public boolean dfs(int[][] graph, int i, boolean[] vis) {
         vis[i] = true;
-        if(s1.contains(i)) {
+        if(s1[i]) {
             for(int ni = 0; ni < graph[i].length; ni++) {
                 int v = graph[i][ni];
-                if(s1.contains(v)) {
+                if(s1[v]) {
                     return false;  
                 } else {
-                    s2.add(v);
+                    s2[v] = true;
                 }
             }
         } else {
             for(int ni = 0; ni < graph[i].length; ni++) {
                 int v = graph[i][ni];
-                if(s2.contains(v)) {
+                if(s2[v]) {
                     return false;  
                 } else {
-                    s1.add(v);
+                    s1[v] = true;
                 }
             }
         }
