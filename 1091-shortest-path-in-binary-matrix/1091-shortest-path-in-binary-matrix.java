1c0109dda2f42ec3ecad8f7c0a5df00be61e8121
Tushar Monga <tusharmongatt@gmail.com>
2022-05-16 11:12:45 +0530

    Time: 47 ms (21.56%), Space: 74.1 MB (8.34%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -0,0 +1,85 @@
+class Solution {
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        if(grid[0][0] != 0 || grid[grid.length - 1][grid[0].length - 1] != 0) {
+            return -1;
+        }
+        
+        // int ans = dfs(grid, 0, 0, new boolean[grid.length][grid[0].length]);
+        // if(ans == Integer.MAX_VALUE) {
+        //     return -1;
+        // }
+        
+        int ans = bfs(grid);
+        
+        return ans;
+    }
+    
+    private int[][] dir = {{0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}};
+    
+    private class Pair {
+        int i;
+        int j;
+        Pair(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+    }
+    
+    private int bfs(int[][] grid) {
+        LinkedList<Pair> que = new LinkedList<>();
+        boolean[][] vis = new boolean[grid.length][grid[0].length];
+        
+        que.add(new Pair(0, 0));
+        boolean reached = false;
+        int level = 0;
+        
+        while(!que.isEmpty() && !reached) {
+            int size = que.size();
+            while(size-- > 0) {
+                Pair rem = que.removeFirst();
+                
+                if(vis[rem.i][rem.j])
+                    continue;
+                vis[rem.i][rem.j] = true;
+                
+                if(rem.i == grid.length - 1 && rem.j == grid[0].length - 1) {
+                    reached = true;
+                }
+                
+                for(int d = 0; d < dir.length; d++) {
+                    int x = dir[d][0] + rem.i;
+                    int y = dir[d][1] + rem.j;
+                    if(x >= 0 && y >= 0 && x < grid.length && y < grid[0].length && !vis[x][y] && grid[x][y] == 0) {
+                        que.addLast(new Pair(x, y));
+                    }
+                }
+            }
+            level++;
+        }
+        
+        if(reached) {
+            return level;
+        } else {
+            return -1;
+        }
+    }
+    
+    private int dfs(int[][] grid, int i, int j, boolean[][] vis) {
+        if(i == grid.length - 1 && j == grid[0].length - 1) {
+            return 1;
+        }
+                
+        int min = Integer.MAX_VALUE;
+        for(int d = 0; d < dir.length; d++) {
+            int x = dir[d][0] + i;
+            int y = dir[d][1] + j;
+            if(x >= 0 && y >= 0 && x < grid.length && y < grid[0].length && !vis[x][y] && grid[x][y] == 0) {
+                vis[x][y] = true;
+                min = Math.min(min, dfs(grid, x, y, vis));
+                vis[x][y] = false;
+            }
+        }
+        
+        return min == Integer.MAX_VALUE ? min : min + 1;
+    }
+}
