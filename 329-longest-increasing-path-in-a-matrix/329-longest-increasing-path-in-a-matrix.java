f4d6c1349098f22246155d336c80d7c9c0c31f1d
Tushar Monga <tusharmongatt@gmail.com>
2022-05-19 13:07:17 +0530

    Time: 15 ms (35.11%), Space: 53.9 MB (51.05%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
@@ -4,14 +4,14 @@ class Solution {
         int dp[][] = new int[matrix.length][matrix[0].length];
         for(int i = 0; i < matrix.length; i++) {
             for(int j = 0; j < matrix[0].length; j++) {
-                int cur = helper(matrix, i, j, dp);
+                int cur = dfs(matrix, i, j, dp);
                 max = Math.max(max, cur);
             }
         }
         return max;
     }
     private int dir[][] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    private int helper(int[][] matrix, int i, int j, int[][] dp) {
+    private int dfs(int[][] matrix, int i, int j, int[][] dp) {
         int max = 0;
         if(dp[i][j] != 0) {
             return dp[i][j];
@@ -20,7 +20,7 @@ class Solution {
             int x = i + dir[d][0];
             int y = j + dir[d][1];
             if(x >= 0 && y >= 0 && x < matrix.length && y < matrix[0].length && matrix[x][y] > matrix[i][j]) {
-                int cur = helper(matrix, x, y, dp);
+                int cur = dfs(matrix, x, y, dp);
                 max = Math.max(max, cur);
             }
         }
