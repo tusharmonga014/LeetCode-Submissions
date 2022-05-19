7f86b82d8b8bb57b174dd63f1df6b8ab413bdfcc
Tushar Monga <tusharmongatt@gmail.com>
2022-05-19 13:08:47 +0530

    Time: 13 ms (47.41%), Space: 53.5 MB (67.69%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
@@ -1,17 +1,20 @@
 class Solution {
+    private int dp[][];
+    private int matrix[][];
     public int longestIncreasingPath(int[][] matrix) {
         int max = 0;
-        int dp[][] = new int[matrix.length][matrix[0].length];
+        dp = new int[matrix.length][matrix[0].length];
+        this.matrix = matrix;
         for(int i = 0; i < matrix.length; i++) {
             for(int j = 0; j < matrix[0].length; j++) {
-                int cur = dfs(matrix, i, j, dp);
+                int cur = dfs(i, j);
                 max = Math.max(max, cur);
             }
         }
         return max;
     }
     private int dir[][] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    private int dfs(int[][] matrix, int i, int j, int[][] dp) {
+    private int dfs(int i, int j) {
         int max = 0;
         if(dp[i][j] != 0) {
             return dp[i][j];
@@ -20,7 +23,7 @@ class Solution {
             int x = i + dir[d][0];
             int y = j + dir[d][1];
             if(x >= 0 && y >= 0 && x < matrix.length && y < matrix[0].length && matrix[x][y] > matrix[i][j]) {
-                int cur = dfs(matrix, x, y, dp);
+                int cur = dfs(x, y);
                 max = Math.max(max, cur);
             }
         }
