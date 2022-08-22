3dc5f5e31995a24562178b11c62bf40feed9436f
Tushar Monga <tusharmongatt@gmail.com>
2022-08-22 16:11:50 +0530

    Time: 2 ms (65.34%), Space: 41.1 MB (67.12%) - LeetHub

---
+++ 342-power-of-four/342-power-of-four.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n <= 0) return false;             // if 0 or negative
+        if(n == 1) return true;              //   4 ^ 0 equals 0
+        if((n & (n - 1)) != 0) return false; // not a power of 2
+        int zerosBeforeMSB = 0;
+        while(n > 0) {
+            n = n >> 1;
+            zerosBeforeMSB++;
+        }
+        if(zerosBeforeMSB % 2 != 0) return true;
+        else return false;
+    }
+}
