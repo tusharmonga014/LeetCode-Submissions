a3e958a7a920f603a45de20b769857c3df16dd6d
Tushar Monga <tusharmongatt@gmail.com>
2022-08-25 15:27:40 +0530

    Time: 4 ms (84.52%), Space: 46.7 MB (50.94%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         if(ransomNote.length() > magazine.length()) return false;
-        int r[] = new int[26];
         int m[] = new int[26];
         for(char c : magazine.toCharArray()) m[c - 'a']++;
         for(char c : ransomNote.toCharArray()) {
