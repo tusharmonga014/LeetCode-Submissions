35fc1fe1e86b3feeee3b416ae00f67f352a4ac05
Tushar Monga <tusharmongatt@gmail.com>
2022-10-13 23:43:10 +0530

    Time: 1124 ms (5.00%), Space: 46.9 MB (7.77%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -3,14 +3,29 @@ class Solution {
         // Integer dp[][][] = new Integer[prices.length][2][k + 1];
         // return f(prices, 0, 1, k, dp);
         
-        int dp[][][] = new int[prices.length + 1][2][k + 1];
-        for(int i = prices.length - 1; i >= 0; i--) {
-            for(int j = 1; j <= k; j++) {
-                dp[i][0][j] = Math.max(prices[i] + dp[i + 1][1][j - 1], dp[i + 1][0][j]);
-                dp[i][1][j] = Math.max(-prices[i] + dp[i + 1][0][j], dp[i + 1][1][j]);
-            }
+        Integer dp[][] = new Integer[prices.length][k + 1];
+        return pep_MEMO(prices, prices.length - 1, k, dp);
+        // int dp[][][] = new int[prices.length + 1][2][k + 1];
+        // for(int i = prices.length - 1; i >= 0; i--) {
+        //     for(int j = 1; j <= k; j++) {
+        //         dp[i][0][j] = Math.max(prices[i] + dp[i + 1][1][j - 1], dp[i + 1][0][j]);
+        //         dp[i][1][j] = Math.max(-prices[i] + dp[i + 1][0][j], dp[i + 1][1][j]);
+        //     }
+        // }
+        // return dp[0][1][k];
+    }
+    
+    public int pep_MEMO(int prices[], int i, int k, Integer[][] dp) {
+        if(i < 0 || k == 0) {
+            return 0;
         }
-        return dp[0][1][k];
+        if(dp[i][k] != null) return dp[i][k];
+        int prevDayK = pep_MEMO(prices, i - 1, k, dp);
+        int todayKth = Integer.MIN_VALUE;
+        for(int idx = 0; idx < i; idx++) {
+            todayKth = Math.max(todayKth, pep_MEMO(prices, idx, k - 1, dp) - prices[idx] + prices[i]);
+        }
+        return dp[i][k] = Math.max(prevDayK, todayKth);
     }
     
     private int f(int[] prices, int idx, int buy, int k, Integer[][][] dp) {
