9bb5fc56aca086e878fccc612c424038345d7702
Tushar Monga <tusharmongatt@gmail.com>
2022-10-13 23:51:12 +0530

    Time: 711 ms (5.00%), Space: 43.4 MB (25.67%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -28,6 +28,22 @@ class Solution {
         return dp[i][k] = Math.max(prevDayK, todayKth);
     }
     
+    public int pep_TAB(int[] prices, int k) {
+        int dp[][] = new int[k + 1][prices.length];
+        for(int i = 1; i <= k; i++) {
+            int max = Integer.MIN_VALUE;
+            for(int day = 0; day < prices.length; day++) {
+                if(day == 0) {
+                    dp[i][day] = 0;
+                } else {
+                    dp[i][day] = Math.max(dp[i][day - 1], max + prices[day]);
+                }
+                max = Math.max(max, dp[i - 1][day] - prices[day]);
+            }
+        }
+        return dp[k][prices.length];
+    }
+    
     private int f(int[] prices, int idx, int buy, int k, Integer[][][] dp) {
         if(idx == prices.length) {
             return 0;
