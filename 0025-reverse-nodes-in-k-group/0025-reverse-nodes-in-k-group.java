cf9a98b3fb909f1c113a7f696778848728e5c027
Tushar Monga <tusharmongatt@gmail.com>
2022-10-15 15:17:00 +0530

    Time: 2 ms (24.42%), Space: 45.4 MB (66.43%) - LeetHub

---
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -0,0 +1,64 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    
+    public int length(ListNode head) {
+        ListNode cur = head;
+        int count = 1;
+        while(cur.next != null) {
+            cur = cur.next;
+            count++;
+        }
+        return count;
+    }
+    
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if(head == null || k == 1)
+            return head;
+        
+        int len = length(head);
+        
+        ListNode h = null;
+        ListNode t = null;
+        
+        if(k > len) {
+            return head;
+        }
+        
+        ListNode cur = head;
+        
+        while(len >= k) {
+            ListNode tt = cur;
+            ListNode forw = null;
+            ListNode prev = t;
+            
+            int tempk = k;
+            
+            while(tempk-- > 0) {
+                forw = cur.next;
+                cur.next = prev;
+                prev = cur;
+                cur = forw;
+            }
+            
+            len -= k;
+            
+            if(h == null) {
+                h = prev;
+                t = tt;
+            }
+            t.next = prev;
+            t = tt;
+        }
+        t.next = cur;
+        return h;
+    }
+}
