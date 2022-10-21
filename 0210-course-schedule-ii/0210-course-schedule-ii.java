2d630ec9538c9256c5524ea11fed14fa64d4de51
Tushar Monga <tusharmongatt@gmail.com>
2022-10-21 16:16:14 +0530

    Time: 10 ms (69.05%), Space: 48.5 MB (72.40%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -3,51 +3,40 @@ class Solution {
         ArrayList<ArrayList<Integer>> gp = new ArrayList<>();
         for(int i = 0; i < numCourses; i++)
             gp.add(new ArrayList<>());
+        
+        int inDegree[] = new int[numCourses];
+        
         for(int i = 0; i < prerequisites.length; i++) {
-            int u = prerequisites[i][0];
-            int v = prerequisites[i][1];
+            int u = prerequisites[i][1];
+            int v = prerequisites[i][0];
             gp.get(u).add(v);
-        }
-        boolean vis[] = new boolean[numCourses];  
-        ArrayList<Integer> stack = new ArrayList<>();
-        boolean cyclef = false;
-        for(int i = 0; i < numCourses; i++) {
-            if(!vis[i]) {
-                boolean cycle[] = new boolean[numCourses];
-                cyclef = cyclef || dfs(gp, i, vis, cycle, stack);
-            }
+            inDegree[v]++;
         }
         
-        if(cyclef) {
+        Queue<Integer> que = new LinkedList<>();
+        
+        for(int i = 0; i < numCourses; i++) {
+            if(inDegree[i] == 0)
+                que.add(i);
+        }
+        
+        int ans[] = new int[numCourses];
+        int idx = 0;
+        while(que.size() > 0) {
+            int s = que.poll();
+            for(int v : gp.get(s)) {
+                inDegree[v]--;
+                if(inDegree[v] == 0)
+                    que.add(v);
+            }
+            ans[idx] = s;
+            idx++;
+        }
+        
+        if(idx < numCourses) {
             return new int[0];
         }
         
-        int ans[] = new int[stack.size()];
-        for(int i = 0; i < stack.size(); i++) {
-            ans[i] = stack.get(i);
-        }
-        
         return ans;
-    }
-    
-    public boolean dfs(ArrayList<ArrayList<Integer>> gp, int src, boolean[] vis, boolean[] cycle, ArrayList<Integer> stack) {
-        vis[src] = true;
-        cycle[src] = true;
-        
-        boolean cyclef = false;
-        for(int v : gp.get(src)) {
-            if(cycle[v]) {
-                return true;
-            } else if(!vis[v]) {
-                cyclef = cyclef || dfs(gp, v, vis, cycle, stack);
-            }
-        }
-        cycle[src] = false;
-        
-        if(!cyclef) {
-            stack.add(src);
-        }
-        
-        return cyclef;
     }
 }
