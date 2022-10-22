a6eb2a1cdccda98632d415e866c71efbeb10b615
Tushar Monga <tusharmongatt@gmail.com>
2022-10-22 22:37:03 +0530

    Added solution - LeetHub

---
+++ N meetings in one room - GFG/n-meetings-in-one-room.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t-- > 0) {
+            String inputLine[] = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+
+            int start[] = new int[n];
+            int end[] = new int[n];
+
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++)
+                start[i] = Integer.parseInt(inputLine[i]);
+
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) 
+                end[i] = Integer.parseInt(inputLine[i]);
+                
+            int ans = new Solution().maxMeetings(start, end, n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution 
+{
+    //Function to find the maximum number of meetings that can
+    //be performed in a meeting room.
+    public static int maxMeetings(int start[], int end[], int n)
+    {
+        int mts[][] = new int[n][2];
+        for(int i = 0; i < n; i++) {
+            mts[i] = new int[]{start[i], end[i]};
+        }
+        Arrays.sort(mts, (a, b) -> {
+            return a[1] - b[1];
+        });
+        
+        int count = 0;
+        
+        int lastMeetingEndTime = -1;
+        
+        for(int i = 0; i < n; i++) {
+            if(mts[i][0] > lastMeetingEndTime) {
+                count++;
+                lastMeetingEndTime = mts[i][1];
+            }
+        }
+        
+        return count;
+    }
+}
