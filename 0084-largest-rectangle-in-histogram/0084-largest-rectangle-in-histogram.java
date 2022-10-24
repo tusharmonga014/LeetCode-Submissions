2fa7b6d63c85415501fe7e0c91e229b4de98d6bc
Tushar Monga <tusharmongatt@gmail.com>
2022-10-25 01:18:26 +0530

    Time: 219 ms (42.46%), Space: 90 MB (57.24%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -7,7 +7,7 @@ class Solution {
         Stack<Integer> st = new Stack<>();
         st.push(-1);
         for(int idx = 0; idx < n; idx++) {
-            while(st.peek() != -1 && heights[st.peek()] >= heights[idx]) {
+            while(st.peek() != -1 && heights[st.peek()] > heights[idx]) {
                 int top = st.pop();
                 
                 int w = idx - st.peek() - 1;
