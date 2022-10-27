8863bcc65ff0bb2a9fcbe3284051cdf3c206425f
Tushar Monga <tusharmongatt@gmail.com>
2022-10-27 18:53:44 +0530

    Time: 25 ms (86.11%), Space: 43 MB (57.61%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,29 +1,43 @@
 class Solution {
     public String longestPalindrome(String s) {
-        boolean dp[][] = new boolean[s.length()][s.length()];
         
-        int max = 0;
-        int ansi = 0;
-        int ansj = 0;
+        int startIdx = 0;
+        int maxLength = 1;
         
-        for(int g = 0; g < s.length(); g++) {
-            for(int i = 0, j = g; j < s.length(); i++, j++) {
-                if(g == 0) {
-                    dp[i][j] = true;
-                } else if(g == 1) {
-                    dp[i][j] = s.charAt(i) == s.charAt(j);
-                } else {
-                    dp[i][j] = s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1];
-                }
-                
-                if(dp[i][j] && max < j - i + 1) {
-                    max = j - i + 1;
-                    ansi = i;
-                    ansj = j;
-                }
+        int len = s.length();
+        int start, end;
+        
+        for(int i = 1; i < len; i++){
+            
+            //even
+            start = i - 1;
+            end = i;
+            while(start >= 0 && end < len && s.charAt(start) == s.charAt(end)){
+                start--;
+                end++;
+            }
+            start++;
+            end--;
+            if(s.charAt(start) == s.charAt(end) && end - start + 1 > maxLength){
+                startIdx = start;
+                maxLength = end - start + 1;
+            }
+            
+            //odd
+            start = i - 1;
+            end = i + 1;
+            while(start >= 0 && end < len && s.charAt(start) == s.charAt(end)){
+                start--;
+                end++;
+            }
+            start++;
+            end--;
+            if(s.charAt(start) == s.charAt(end) && end - start + 1 > maxLength){
+                startIdx = start;
+                maxLength = end - start + 1;
             }
         }
         
-        return max > 0 ? s.substring(ansi, ansj + 1) : "";
+        return s.substring(startIdx, startIdx + maxLength);
     }
 }
